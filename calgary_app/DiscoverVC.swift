bb8b65023aa5561ae1e5e340d67c5713bcda3fcd
Jaf Crisologo <jafcrisologo@Jafs-MacBook-Pro.local>
2016-05-29 23:59:21 -0600

    First commit

---
+++ calgary_app/DiscoverVC.swift
@@ -0,0 +1,40 @@
+//
+//  DiscoverVC.swift
+//  calgary_app
+//
+//  Created by Jaf Crisologo on 2016-04-03.
+//  Copyright © 2016 Modium Design. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func backPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- calgary_app/ViewController.swift
+++ calgary_app/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var discoverBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        discoverBtn.layer.cornerRadius = 2.5
+        
     }
 
     override func didReceiveMemoryWarning() {
